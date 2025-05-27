12245106d5262131753f1a80a7326bafa14b3bd5
cyamoto <54260520+cyamoto@users.noreply.github.com>
2025-05-28 07:57:35 +0900

    5/28commit1

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className='container'>
       <img src={logo} alt="" className='logo' />
       <ul>
         <li>Home</li>
@@ -12,7 +12,7 @@ const Navbar = () => {
         <li>About us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li>Contact us</li>
+        <li><button className='btn'>Contact us</button></li>
       </ul>    
     </nav>
   )

fa8485e732e8749b8e25bbd109ba23dd8d8ec736
cyamoto <54260520+cyamoto@users.noreply.github.com>
2025-05-29 08:05:47 +0900

    5/29 commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark_arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>aaaaaa</h1>
+        <p>cccccccccccccccccccccccccccccccc</p>
+        <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+      </div>
       
     </div>
   )

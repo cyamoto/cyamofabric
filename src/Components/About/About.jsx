3f8f69ac3365a1d1518797e75fd061723bc5c4eb
cyamoto <54260520+cyamoto@users.noreply.github.com>
2025-06-12 07:42:57 +0900

    6/12 commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = () => {
+  return(
+    <div className='about'>
+      <div className="about-left">
+        <img src={about_img} alt="" className='about_img'/>
+        <img src={play_icon} alt="" className='play-icon'/>
+      </div>
+      <div className="about-right">
+        <h3>cccccccccccccccc</h3>
+        <h2>mmmmmmmmmmmmmmmmmmmmmmmm</h2>
+        <p>gggggggggggggggggggggggggggggggggggggggg</p>
+        <p>ffffffffffffffffffffffffffffffffffffffff</p>
+        <p>gggggggggggggggggggggggggggggggggggggggg</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
